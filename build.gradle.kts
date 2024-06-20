bb2580eac592a6939c2b66ffab9b8b03ed52abe7
Nguyen Minh Tho <minhthoj27@icloud.com>
2024-06-20 22:41:18 +0700

    Init Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+val version_major: String by project
+val version_minor: String by project
+val version_patch: String by project
+val project_id: String by project
+
+repositories {
+    mavenCentral()
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "com.github.johnrengelman.shadow")
+
+    group = project_id
+    version = "$version_major.$version_minor.$version_patch"
+
+    repositories {
+        mavenCentral()
+    }
+
+    val platform = project.name
+
+    base {
+        archivesName.set("$project_id-$platform")
+    }
+
+    java {
+        withSourcesJar()
+        toolchain.languageVersion = JavaLanguageVersion.of(17) // Kotlin can read this line
+    }
+
+    tasks {
+        jar {
+            archiveClassifier.set("noshade")
+        }
+        shadowJar {
+            archiveClassifier.set("")
+            archiveBaseName.set(archiveBaseName.get())
+            archiveVersion.set(version.toString())
+        }
+        build {
+            dependsOn(shadowJar)
+        }
+        processResources {
+            val project_name: String by project
+            val project_license: String by project
+            val project_author: String by project
+            val project_description: String by project
+
+            filesMatching(listOf("META-INF/neoforge.mods.toml", "fabric.mod.json", "plugin.yml")) {
+                expand(mapOf( "project_id" to project_id,
+                    "project_name" to project_name,
+                    "project_license" to project_license,
+                    "project_version" to version,
+                    "project_author" to project_author,
+                    "project_description" to project_description))
+            }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "DailyDadJokes"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
